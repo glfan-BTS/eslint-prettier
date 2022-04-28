49f2a58bd8e667b3adc5103fbc64fae037b6fc94
范国莲 <wb_fanguolian@fanguoliandeMacBook-Pro.local>
2022-04-28 12:07:32 +0800

    feat: 新增eslint-prettier项目

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false, // 缩进不使用tab，使用空格
+  semi: true, // 结尾不使用分号
+  singleQuote: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  trailingComma: 'all',
+  arrowParens: 'always',
+};
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # eslint-prettier
+
 Eslint+Prettier+husky+lint-staged
+
+# 注意 husky 必须要在根目录下执行 对于当前项目目录
+
+https://jo-hnny.github.io/p/huskyv7%E4%BD%BF%E7%94%A8%E6%8C%87%E5%8D%97/
+
+```js
+ npx husky install ./eslint-demo/.husky
+```
